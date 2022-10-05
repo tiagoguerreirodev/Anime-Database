02e7b26503d586aeb5498989b0eab3c91610c9ea
Tiago Guerreiro <Tiago.Guerreiro@br.experian.com>
2022-10-05 17:08:05 -0300

    Refactor document, uncommented CORS

--- AnimeAPI/services/GetAllUserAnimeService.py
+++ AnimeAPI/services/GetAllUserAnimeService.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from repositories.AnimeDBRepository import AnimeDBRepository
 
 
@@ -5,4 +6,6 @@ class GetAllUserAnimeService():
     async def execute(self):
         anime_db_repository = AnimeDBRepository()
         response = await anime_db_repository.get_all_animes()
+        if not response:
+            raise HTTPException(404, "Não existem animes no banco de dados")
         return response
