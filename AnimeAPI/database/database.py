53b505570f12f9401777ba7c7fb54de76193c3f6
Tiago Guerreiro <Tiago.Guerreiro@br.experian.com>
2022-10-05 19:49:58 -0300

    Fixed database not connecting on manual deploy

--- AnimeAPI/database/database.py
+++ AnimeAPI/database/database.py
@@ -1,6 +1,8 @@
 import motor.motor_asyncio
+from os import environ as env
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://db:27017')
+
+client = motor.motor_asyncio.AsyncIOMotorClient(f"mongodb://{'db' if env.get('RUNNING_IN_DOCKER') else 'localhost'}:27017")
 
 database = client.AnimeDB
 anime_collection = database.anime
